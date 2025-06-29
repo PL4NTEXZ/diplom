abddc8c5447c8e7b8ce32f762add70963d7a99f3
pl4ntexz <pl4ntexz@yandex.ru>
2025-06-29 19:28:31 +0500

    diplom

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+service_account_key_file_ya = "/home/pl4ntexz/sukhopalov.json"
+cloud_id_ya = "b1gi844n17tqndv9mo4j"
+folder_id_ya = "b1g5gb7s84bn8b5el62r"
+image_id_ya = "fd85u0rct32prepgjlv0"
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "service_account_key_file_ya" {
+  type        = string
+  description = "Path to Yandex.Cloud service account key file"
+}
+
+variable "cloud_id_ya" {
+  type        = string
+  description = "Yandex.Cloud Cloud ID"
+}
+
+variable "folder_id_ya" {
+  type        = string
+  description = "Yandex.Cloud Folder ID"
+}
+
+variable "image_id_ya" {
+  type        = string
+  description = "Yandex.Cloud Folder ID"
+}
